2c456081d6df5634edff157c4a72b63715ace1f0
Dmitry <40204213+Notowlmenot@users.noreply.github.com>
2023-05-13 08:26:23 +1100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+$(window).on("load",function() {
+    $(".loader-container").fadeOut(1000);
+});
+
+$('#opred-btn').click(function() {
+    window.location.href = './pages/opred.html';
+    return false;
+});
